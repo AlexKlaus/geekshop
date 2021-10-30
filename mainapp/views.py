1853c887a4165c8eef90c17e689129795f120036
Alex <krounix@gmail.com>
2021-10-30 19:11:30 +0300

    Lesson 2. JSON content

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,51 +1,22 @@
 import datetime
-
+import json
 from django.shortcuts import render
 
 
 def main(request):
     title = "главная"
-    products = [
-        {
-            "name": "Отличный стул",
-            "desc": "Расположитесь комфортно.",
-            "image_src": "product-1.jpg",
-            "image_href": "/product/1/",
-            "alt": "продукт 1",
-        },
-        {
-            "name": "Стул повышенного качества",
-            "desc": "Не оторваться.",
-            "image_src": "product-2.jpg",
-            "image_href": "/product/2/",
-            "alt": "продукт 2",
-        },
-    ]
+    with open('./static/content/main/products.json') as f:
+        products = json.load(f)
     content = {"title": title, "products": products}
     return render(request, "mainapp/index.html", content)
 
 
 def products(request):
     title = "продукты"
-    links_menu = [
-        {"href": "products_all", "name": "все"},
-        {"href": "products_home", "name": "дом"},
-        {"href": "products_office", "name": "офис"},
-        {"href": "products_modern", "name": "модерн"},
-        {"href": "products_classic", "name": "классика"},
-    ]
-    same_products = [
-        {"name": "Прекрасный стул", "desc": "Не оторваться.",
-            "image_src": "product-11.jpg", "alt": "продукт 11"},
-        {"name": "Стул повышенного качества", "desc": "Комфортно.",
-            "image_src": "product-21.jpg", "alt": "продукт 21"},
-        {
-            "name": "Стул премиального качества",
-            "desc": "Просто попробуйте.",
-            "image_src": "product-31.jpg",
-            "alt": "продукт 31",
-        },
-    ]
+    with open('./static/content/products/links_menu.json') as f:
+        links_menu = json.load(f)
+    with open('./static/content/products/same_products.json') as f:
+        same_products = json.load(f)
     content = {"title": title, "links_menu": links_menu,
                "same_products": same_products}
     return render(request, "mainapp/products.html", content)
@@ -54,22 +25,8 @@ def products(request):
 def contact(request):
     title = "о нас"
     visit_date = datetime.datetime.now()
-    locations = [
-        {"city": "Москва", "phone": "+7-888-888-8888",
-            "email": "info@geekshop.ru", "address": "В пределах МКАД"},
-        {
-            "city": "Екатеринбург",
-            "phone": "+7-777-777-7777",
-            "email": "info_yekaterinburg@geekshop.ru",
-            "address": "Близко к центру",
-        },
-        {
-            "city": "Владивосток",
-            "phone": "+7-999-999-9999",
-            "email": "info_vladivostok@geekshop.ru",
-            "address": "Близко к океану",
-        },
-    ]
+    with open('./static/content/contact/contact.json') as f:
+        locations = json.load(f)
     content = {"title": title, "visit_date": visit_date,
                "locations": locations}
     return render(request, "mainapp/contact.html", content)
