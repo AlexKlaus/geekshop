ba68586dff23cd70a29f6b638aa57933e61fd708
AlexKlaus <60125111+AlexKlaus@users.noreply.github.com>
2021-11-03 08:03:27 +0300

    Lesson 2 (#2)
    * Lesson 2. Templates
    
    * Lesson 2. Context for templates
    
    * Lesson 2. Template context
    
    * Lesson 2. Repeat and filters
    
    * Lesson 2. JSON content

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,32 @@
+import datetime
+import json
 from django.shortcuts import render
 
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    title = "главная"
+    with open('./static/content/main/products.json') as f:
+        products = json.load(f)
+    content = {"title": title, "products": products}
+    return render(request, "mainapp/index.html", content)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    title = "продукты"
+    with open('./static/content/products/links_menu.json') as f:
+        links_menu = json.load(f)
+    with open('./static/content/products/same_products.json') as f:
+        same_products = json.load(f)
+    content = {"title": title, "links_menu": links_menu,
+               "same_products": same_products}
+    return render(request, "mainapp/products.html", content)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    title = "о нас"
+    visit_date = datetime.datetime.now()
+    with open('./static/content/contact/contact.json') as f:
+        locations = json.load(f)
+    content = {"title": title, "visit_date": visit_date,
+               "locations": locations}
+    return render(request, "mainapp/contact.html", content)
