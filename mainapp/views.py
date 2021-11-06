eb5e0e5992c732901e93a0a54b7c0fcc96b609c5
Alex <krounix@gmail.com>
2021-11-06 16:10:38 +0300

    Lesson 3. Models in context

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,32 +1,49 @@
-import datetime
-import json
+from django.conf import settings
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Product, ProductCategory
 
 
 def main(request):
     title = "главная"
-    with open('./static/content/main/products.json') as f:
-        products = json.load(f)
-    content = {"title": title, "products": products}
+
+    products = Product.objects.all()
+
+    content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
     return render(request, "mainapp/index.html", content)
 
 
 def products(request):
     title = "продукты"
-    with open('./static/content/products/links_menu.json') as f:
-        links_menu = json.load(f)
-    with open('./static/content/products/same_products.json') as f:
-        same_products = json.load(f)
-    content = {"title": title, "links_menu": links_menu,
-               "same_products": same_products}
+    links_menu = ProductCategory.objects.all()
+    same_products = Product.objects.all()
+    content = {
+        "title": title,
+        "links_menu": links_menu,
+        "same_products": same_products,
+        "media_url": settings.MEDIA_URL,
+    }
     return render(request, "mainapp/products.html", content)
 
 
 def contact(request):
     title = "о нас"
-    visit_date = datetime.datetime.now()
-    with open('./static/content/contact/contact.json') as f:
-        locations = json.load(f)
-    content = {"title": title, "visit_date": visit_date,
-               "locations": locations}
+    visit_date = timezone.now()
+    locations = [
+        {"city": "Москва", "phone": "+7-888-888-8888", "email": "info@geekshop.ru", "address": "В пределах МКАД"},
+        {
+            "city": "Екатеринбург",
+            "phone": "+7-777-777-7777",
+            "email": "info_yekaterinburg@geekshop.ru",
+            "address": "Близко к центру",
+        },
+        {
+            "city": "Владивосток",
+            "phone": "+7-999-999-9999",
+            "email": "info_vladivostok@geekshop.ru",
+            "address": "Близко к океану",
+        },
+    ]
+    content = {"title": title, "visit_date": visit_date, "locations": locations}
     return render(request, "mainapp/contact.html", content)
